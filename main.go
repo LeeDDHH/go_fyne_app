1665180248e57d67687b5f4d3aed74120feef47a
DongHeon Lee <niche3600@gmail.com>
2022-05-03 05:35:01 +0900

    チェックボックス

--- main.go
+++ main.go
@@ -20,11 +20,9 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
 	l := widget.NewLabel("Hello Fyne")
-	e := widget.NewEntry()
-	darkTheme := theme.DarkTheme()
-	lightTheme := theme.LightTheme()
-	currentTheme := darkTheme
 
+	// エントリー
+	e := widget.NewEntry()
 	e.SetText("0")
 	eCulculate := func() {
 		n, _ := strconv.Atoi(e.Text)
@@ -32,6 +30,10 @@ func main() {
 	}
 	b := widget.NewButton("Click", eCulculate)
 
+	// テーマ
+	darkTheme := theme.DarkTheme()
+	lightTheme := theme.LightTheme()
+	currentTheme := darkTheme
 	changeTheme := func() {
 		changedTheme := darkTheme
 		if currentTheme == changedTheme {
@@ -44,11 +46,23 @@ func main() {
 	}
 	changeThemeButton := widget.NewButton("Change Theme", changeTheme)
 
+	// チェックボックス
+	checkEvent := func(f bool){
+		if f {
+			l.SetText("checked!")
+		} else {
+			l.SetText("not checked")
+		}
+	}
+	c := widget.NewCheck("checkbox", checkEvent)
+	c.SetChecked(true)
+
 	widgetSet := widget.NewVBox(
 		l,
 		e,
 		b,
 		changeThemeButton,
+		c,
 	)
 	w.SetContent(widgetSet)
 
