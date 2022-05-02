59871e3d35526724679353d28b5ca3a7b56a63e5
DongHeon Lee <niche3600@gmail.com>
2022-05-03 04:30:32 +0900

    ボタン配置とカウントアップ

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"strconv"
+
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
 )
 
 func main() {
+	c := 0
 	a := app.New()
-
 	w := a.NewWindow("Hello")
-	// widgetSet := widget.NewVBox(
-	widgetSet := widget.NewHBox(
-		widget.NewLabel("Hello Fyne"),
-		widget.NewLabel("second text"),
+	l := widget.NewLabel("Hello Fyne")
+	cCount := func(){
+		c++
+		l.SetText("count: " + strconv.Itoa(c))
+	}
+	b := widget.NewButton("Click", cCount)
+	
+	widgetSet := widget.NewVBox(
+		l,
+		b,
 	)
 	w.SetContent(widgetSet)
 
